8dffbb4352833eb19b3d1d4e2b200b53f292db9e
Tomas Trajan <tomas.trajan@gmail.com>
2015-07-20 18:31:04 +0200

    Problem 2 - Even Fibonacci numbers, removed lib directory, moved implementation to test file

--- test/100/problem-1-multiples-3-5.js
+++ test/100/problem-1-multiples-3-5.js
@@ -2,18 +2,32 @@
 
 import { assert } from 'chai';
 
-import * as solution from '../../lib/100/problem-1-multiples-3-5';
+describe('Problem 1 - Multiples of 3 and 5', function () {
 
+    it(`
 
-describe('Problem 1 - Multiples of 3 and 5', function() {
+        If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+        The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
-    it('If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. ' +
-        'The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.', function() {
-
-        assert.equal(solution.run(), 233168);
-
+    `, function () {
+        assert.equal(solution(), 233168);
     });
 
 });
+
+function solution() {
+
+    let limit = 1;
+    let result = 0;
+
+    while(limit < 1000) {
+        if (limit % 3 === 0 || limit % 5 ===0) {
+            result += limit;
+        }
+        limit++;
+    }
+
+    return result;
+}
 
 
